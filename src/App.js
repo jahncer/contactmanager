beac976222ed4b69a425f063398d2e0f81fab025
Jahncer <jahncer@gmail.com>
2020-01-01 22:18:56 -0500

    Added bottstrap and state to contacts component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from "react";
-import "./App.css";
-import Contact from "./components/Contact";
-import Header from "./components/Header";
+import React from 'react';
+import './App.css';
+import Contacts from './components/Contacts';
+import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <Header branding="Contact Manager" />
-      <Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-5555" />
+      <div className="container">
+        <Contacts />
+      </div>
     </div>
   );
 }
