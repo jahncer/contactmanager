422e3ed9b073e9878407cb8d15f235e7df72a9ac
Jahncer <jahncer@gmail.com>
2020-01-07 19:08:39 -0500

    added delete option to contact component

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,38 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import Contact from './Contact';
+import { Consumer } from '../context';
 
 class Contacts extends Component {
-  state = {
-    contacts: [
-      {
-        id: 1,
-        name: 'John Doe',
-        email: 'jdoe@gmail.com',
-        phone: '555-555-5555'
-      },
-      {
-        id: 2,
-        name: 'Karen Williams',
-        email: 'karen@gmail.com',
-        phone: '333-333-3333'
-      },
-      {
-        id: 3,
-        name: 'Henry Johnson',
-        email: 'henry@gmail.com',
-        phone: '444-444-4444'
-      }
-    ]
-  };
-
   render() {
-    const { contacts } = this.state;
     return (
-      <div>
-        {contacts.map(contact => (
-          <Contact key={contact.id} contact={contact} />
-        ))}
-      </div>
+      <Consumer>
+        {value => {
+          const { contacts } = value;
+          return (
+            <Fragment>
+              {contacts.map(contact => (
+                <Contact key={contact.id} contact={contact} />
+              ))}
+            </Fragment>
+          );
+        }}
+      </Consumer>
     );
   }
 }
